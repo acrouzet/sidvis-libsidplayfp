53ca2cdc8061f4b9da68291e06f6070a5510f25a
Acro <47704566+acrouzet@users.noreply.github.com>
2024-10-01 11:14:39 -0400

    Add new README and msys2.sh

---
+++ msys2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+pacman -S mingw-w64-ucrt-x86_64-autotools
+pacman -S mingw-w64-ucrt-x86_64-gcc
+pacman -S mingw-w64-ucrt-x86_64-xa65
+autoreconf -i
+./configure --enable-hardsid
+make
+make install
+git clone https://github.com/libsidplayfp/sidplayfp.git
+cd sidplayfp
+autoreconf -i
+./configure
+make
+make install
